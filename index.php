e5b470866309ae911c2675c0e69f5a0b69adbef9
Malikabhc <malika.bouhcine@gmail.com>
2018-07-27 14:45:54 +0200

    correction

--- index.php
+++ index.php
@@ -8,13 +8,16 @@
     <title>Exercice 1/2</title>
   </head>
   <body>
-    <?php
-      if ($age >= 18) {
-        echo 'Vous êtes majeur';
-      }
-      else {
-        echo 'Vous êtes mineur';
-      }
-    ?>
+    <p>
+      <?php
+        if ($age < 0) {
+          echo 'Age incorrect';
+        } elseif ($age >= 18) {
+          echo 'Vous êtes majeur';
+        } else {
+          echo 'Vous êtes mineur';
+        }
+      ?>
+    </p>
   </body>
 </html>
